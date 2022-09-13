c5f33cb4dad587c951ca0c8d78eec2cab57a1f92
Cheah KK <ckarkeong@gmail.com>
2022-09-14 00:33:37 +0800

    Update report entity

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -6,5 +6,23 @@ export class Report {
   id: number;
 
   @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  mileage: number;
+
+  @Column()
+  lng: number;
+
+  @Column()
+  lat: number;
+
+  @Column()
   price: number;
 }
